17bbe49a4640171cf42c16768947edc2731866e1
DebaKaran <karan.deba@gmail.com>
2025-08-17 23:26:15 +0530

    feat(consumer): add multiple Kafka listeners with differsameent group IDs
    - Implemented an additional @KafkaListener for `test-topic` using a same group ID
    - Allows multiple independent consumer groups to consume the same topic messages
    - Ensures each group receives all messages from the topic

--- kafka-consumer/src/main/java/com/dk/kafkaconsumer/services/KafkaMessageListner.java
+++ kafka-consumer/src/main/java/com/dk/kafkaconsumer/services/KafkaMessageListner.java
@@ -10,8 +10,21 @@ public class KafkaMessageListner {
 
     Logger logger = LoggerFactory.getLogger(KafkaMessageListner.class);
 
+    // Consumer 1
     @KafkaListener(topics = "test-topic", groupId = "test-group")
-    public void consume(String message) {
-        logger.info("Consumer consumed the message: "+message);
+    public void consumeGroup1(String message) {
+        logger.info("Consumer1 received: {}", message);
+    }
+
+    // Consumer 2
+    @KafkaListener(topics = "test-topic", groupId = "test-group")
+    public void consumeGroup2(String message) {
+        logger.info("Consumer2 received: {}", message);
+    }
+
+    // Consumer 3
+    @KafkaListener(topics = "test-topic", groupId = "test-group")
+    public void consumeGroup3(String message) {
+        logger.info("Consumer3 received: {}", message);
     }
 }
